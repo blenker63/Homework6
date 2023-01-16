19b609979cd37b02b4f282e4177d8eff3afb3bff
vaceslavkameronov <slka@mail.ru>
2023-01-16 20:47:48 +0400

    решение задач 5-7

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,21 @@ public class Main {
         task2();
         task3();
         task4();
-        //task5();
+        task5();
+        task6();
+        task7();
     }
 
     public static void task1() {
         System.out.println("Задача 1");
-        for (int i = 1; i <= 10; i = i + 1) {
+        for (int i = 1; i <= 10; i++ ) {
             System.out.println(+i);
         }
     }
 
     public static void task2() {
         System.out.println("Задача 2");
-        for (int i = 10; i >= 1; i = i - 1) {
+        for (int i = 10; i >= 1; i --) {
             System.out.println(+i);
         }
     }
@@ -31,6 +33,26 @@ public class Main {
     public static void task4() {
         System.out.println("Задача 4");
         for (int i = 10; i >= -10; i = i - 1) {
-            System.out.println( + i );}
+            System.out.println( + i );
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println( i + " год является високосным"); // необходима проверка на каждые 100 и 400 лет
+        }
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println( +i );
+        }
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println( +i );
+        }
     }
 }
