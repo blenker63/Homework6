71d3eaac7b067e46f218d85b69317254a4fef3bb
vaceslavkameronov <slka@mail.ru>
2023-01-17 20:11:26 +0400

    переделан вывод чисел из столбика в строчку в задачах №1, 2, 3, 4, 6, 7.

--- src/Main.java
+++ src/Main.java
@@ -16,29 +16,33 @@ public class Main {
     public static void task1() {
         System.out.println("Задача 1");
         for (int i = 1; i <= 10; i++) {
-            System.out.println( i );
+            System.out.print(i + " ");
         }
+        System.out.println();
     }
 
     public static void task2() {
         System.out.println("Задача 2");
         for (int i = 10; i >= 1; i--) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println();
     }
 
     public static void task3() {
         System.out.println("Задача 3");
         for (int i = 0; i <= 17; i = i + 2) {
-            System.out.println(+i);
+            System.out.print(i + " ");
         }
+        System.out.println();
     }
 
     public static void task4() {
         System.out.println("Задача 4");
         for (int i = 10; i >= -10; i = i - 1) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println();
     }
 
     public static void task5() {
@@ -51,15 +55,17 @@ public class Main {
     public static void task6() {
         System.out.println("Задача 6");
         for (int i = 7; i <= 98; i = i + 7) {
-            System.out.println(i);
+            System.out.print (i + " ");
         }
+        System.out.println();
     }
 
     public static void task7() {
         System.out.println("Задача 7");
         for (int i = 1; i <= 512; i = i * 2) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println();
     }
 
     public static void task8() {
